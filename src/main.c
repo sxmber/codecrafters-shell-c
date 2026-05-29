a893166381ddbb38b5d139fb964d777387fdc41a
Michael <williamsmd002@gmail.com>
2026-05-29 00:12:01 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,9 +8,15 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
    while(1){
     
-    char *paths = getenv("PATH");
-    printf("%s", paths);
+    //char *paths = getenv("PATH");
+    //printf("%s", paths);
     printf("$ ");
+    char *builtins[] = {
+      "echo",
+      "exit",
+      "type",
+      NULL
+    };
     //take user input as a variable then print $variable: command not found
     char command[1024];
     fgets(command, sizeof(command), stdin);
@@ -22,23 +28,34 @@ int main(int argc, char *argv[]) {
     //Strings are pointers in C. A pointer is a memory address. %s will take the memory address and print the value.
     //* takes a memory address and grabs the value stored there.
     //& takes a variable/value and reveals it memory address.
-    else if (strncmp(command, "echo", 3) == 0){ //only looking at the first 3 characters of $command and if it is == to "echo" then run..
+    if (strncmp(command, "echo", 4) == 0){ //only looking at the first 3 characters of $command and if it is == to "echo" then run..
       printf("%s\n", command + 5);
     }
-    else if(strncmp(command, "type", 3) == 0){
-      if(strncmp(command + 5,"echo", 5) == 0){
-        printf("echo is a shell builtin\n");
+    if(strncmp(command, "type", 4) == 0){ 
+      char *target = command + 5; 
+      int found = 0;
+
+      for (int i =0; builtins[i] != NULL; i++){
+        if(strcmp(target, builtins[i]) == 0) {
+          printf("%s is a shell builtin\n", target);
+          found = 1;
+          break;
+        }
+        
       }
-      else if(strncmp(command + 5, "exit", 5) == 0){
-        printf("exit is a shell builtin\n");
+      //if builtin not found, check PATH
+      //Get path, split it by : and save each one to an array?
+      //loop through each iteration and check the direcory for a match of what user inputed
+
+      if(!found){
+        char *path = getenv("PATH");
+        char *splitPath = strtok(path, ":");
+        printf("path: %s\nsplitpath: %s",path,splitPath);
       }
-      else if(strncmp(command + 5, "type", 5) == 0){
-        printf("type is a shell builtin\n");
-      }
-      else {
-        printf("%s: not found\n", command + 5);
-      }
-    }
+     
+    //else target not found 
+      
+    } 
     else {
     printf("%s: command not found\n" ,command);
     } 
